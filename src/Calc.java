3ef3a2adbeba4b817f04fa527ba8009e7657ec21
AzarovaNatalia <aleksei.sozin@mail.ru>
2020-10-03 10:28:57 +0600

    lab03

---
+++ src/Calc.java
@@ -0,0 +1,31 @@
+public class Calc {
+
+    public static void main(String[] args) {
+        try {
+//           String consoleLine = "-1.5 + 1e-02";
+//           String[] args = consoleLine.split(" ");
+            double x = Double.parseDouble(args[0]);
+            char operation = args[1].charAt(0);
+            double y = Double.parseDouble(args[2]);
+            System.out.println(calc(x, y, operation));
+        }catch(NumberFormatException ex){
+            System.out.println("Invalid number format");
+        }
+    }
+
+    static double calc(double x, double y, char operation) {
+        switch (operation) {
+            case '+':
+                return x + y;
+            case '-':
+                return x - y;
+            case '*':
+                return x * y;
+            case '/':
+                return x / y;
+            default:
+                return Double.NaN;
+        }
+
+    }
+}
